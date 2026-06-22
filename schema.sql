4b40e67e5e00922305d9b36d31c9b9081135ad15
mjsovich <sw52457@umbc.edu>
2026-06-22 11:02:38 -0400

    Skeleton for attacks in schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,7 @@
 -- this file will contain all the commands to set up the database
 CREATE TABLE attacks {
-
-
+    name VARCHAR(30); -- Name
+    damage INT; -- Base damage
+    type VARCHAR(20); -- Grass/Water/Etc
+    style VARCHAR(20);  -- Special/Physical
 }
