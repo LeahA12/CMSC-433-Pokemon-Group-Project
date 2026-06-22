8aed9e7a86ce6db0c75d526fbc409369f3f67c53
mjsovich <sw52457@umbc.edu>
2026-06-22 11:12:57 -0400

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,6 @@
 -- this file will contain all the commands to set up the database
-CREATE TABLE attacks {
+CREATE TABLE attacks 
+    id INT AUTO_INCREMENT PRIMARY KEY;
     name VARCHAR(30); -- Name
     amount INT; -- Base damage
     type VARCHAR(20); -- Grass/Water/Etc
@@ -8,16 +9,16 @@ CREATE TABLE attacks {
 }
 
 CREATE TABLE pokemon {
-    index INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30);
-    type1 VARCHAR(20);
-    type2 VARCHAR(20);
-    hp INT;
-    attack INT;
-    defense INT;
-    spAttack INT;
-    spDefense INT;
-    speed INT;
-    generation INT;
-    legendary BOOLEAN;
+    id INT AUTO_INCREMENT PRIMARY KEY; -- Index number
+    name VARCHAR(30); -- Pokemon name
+    type1 VARCHAR(20); -- Pokemon primary type
+    type2 VARCHAR(20); -- Pokemon secondary type (optional)
+    hp INT; -- Hit points
+    attack INT; -- Base attack
+    defense INT; -- Base defense
+    spAttack INT; -- Base special attack
+    spDefense INT; -- Base special defense
+    speed INT; -- Base speed
+    generation INT; -- Pokemon generation
+    legendary BOOLEAN; -- T/F is legendary
 }
