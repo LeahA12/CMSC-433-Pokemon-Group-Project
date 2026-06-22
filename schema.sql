4761d434045702a3356f90ec91a1801ea652dd9a
mjsovich <sw52457@umbc.edu>
2026-06-22 11:23:20 -0400

    Fixed table formats in schema.sql

--- schema.sql
+++ schema.sql
@@ -1,24 +1,24 @@
 -- this file will contain all the commands to set up the database
-CREATE TABLE attacks {
-    id INT AUTO_INCREMENT PRIMARY KEY;
-    name VARCHAR(30); -- Name
-    amount INT; -- Base damage
-    type VARCHAR(20); -- Grass/Water/Etc
-    style VARCHAR(20);  -- Special/Physical
-    status VARCHAR(20); -- Poison opponent, paralyze opponent, etc.
-}
+CREATE TABLE attacks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30), -- Name
+    amount INT, -- Base damage
+    type VARCHAR(20), -- Grass/Water/Etc
+    style VARCHAR(20),  -- Special/Physical
+    status VARCHAR(20), -- Poison opponent, paralyze opponent, etc.
+);
 
-CREATE TABLE pokemon {
-    id INT AUTO_INCREMENT PRIMARY KEY; -- Index number
-    name VARCHAR(30); -- Pokemon name
-    type1 VARCHAR(20); -- Pokemon primary type
-    type2 VARCHAR(20); -- Pokemon secondary type (optional)
-    hp INT; -- Hit points
-    attack INT; -- Base attack
-    defense INT; -- Base defense
-    spAttack INT; -- Base special attack
-    spDefense INT; -- Base special defense
-    speed INT; -- Base speed
-    generation INT; -- Pokemon generation
-    legendary BOOLEAN; -- T/F is legendary
-}
+CREATE TABLE pokemon (
+    id INT AUTO_INCREMENT PRIMARY KEY, -- Index number
+    name VARCHAR(30), -- Pokemon name
+    type1 VARCHAR(20), -- Pokemon primary type
+    type2 VARCHAR(20), -- Pokemon secondary type (optional)
+    hp INT, -- Hit points
+    attack INT, -- Base attack
+    defense INT, -- Base defense
+    spAttack INT, -- Base special attack
+    spDefense INT, -- Base special defense
+    speed INT, -- Base speed
+    generation INT, -- Pokemon generation
+    legendary BOOLEAN, -- T/F is legendary
+);
