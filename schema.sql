885debcbebdabdf0bbc96b58c63305f9208d134d
mjsovich <sw52457@umbc.edu>
2026-06-22 11:55:25 -0400

    Foreign keys in tables in schema.sql

--- schema.sql
+++ schema.sql
@@ -21,24 +21,29 @@ CREATE TABLE pokemon (
     speed INT NOT NULL, -- Base speed
     generation INT NOT NULL, -- Pokemon generation
     legendary BOOLEAN NOT NULL,-- T/F is legendary
-    move1 VARCHAR(30) NOT NULL,
-    move2 VARCHAR(30) NOT NULL,
-    move3 VARCHAR(30) DEFAULT NULL,
-    move4 VARCHAR(30) DEFAULT NULL
+    
+    move1_id INT NOT NULL,
+    move2_id INT NOT NULL,
+    move3_id INT DEFAULT NULL,
+    move4_id INT DEFAULT NULL,
+    CONSTRAINT fk_move1_id FOREIGN KEY (move1_id) REFERENCES attacks(attack_id),
+    CONSTRAINT fk_move2_id FOREIGN KEY (move2_id) REFERENCES attacks(attack_id),
+    CONSTRAINT fk_move3_id FOREIGN KEY (move3_id) REFERENCES attacks(attack_id),
+    CONSTRAINT fk_move4_id FOREIGN KEY (move4_id) REFERENCES attacks(attack_id)
 );
 
 CREATE TABLE player (
     player_id INT AUTO_INCREMENT PRIMARY KEY,
-    poke1_id INT,
-    poke2_id INT,
-    poke3_id INT,
-    poke4_id INT,
-    poke5_id, INT,
-    poke6_id, INT,
-    CONSTRAINT poke1 FOREIGN KEY (poke1_id) REFERENCES pokemon(poke_id),
-    CONSTRAINT poke2 FOREIGN KEY (poke2_id) REFERENCES pokemon(poke_id),
-    CONSTRAINT poke3 FOREIGN KEY (poke3_id) REFERENCES pokemon(poke_id),
-    CONSTRAINT poke4 FOREIGN KEY (poke4_id) REFERENCES pokemon(poke_id),
-    CONSTRAINT poke5 FOREIGN KEY (poke5_id) REFERENCES pokemon(poke_id),
-    CONSTRAINT poke6 FOREIGN KEY (poke6_id) REFERENCES pokemon(poke_id)
+    poke1_id INT NOT NULL,
+    poke2_id INT NOT NULL,
+    poke3_id INT NOT NULL,
+    poke4_id INT DEFAULT NULL,
+    poke5_id INT DEFAULT NULL,
+    poke6_id INT DEFAULT NULL,
+    CONSTRAINT fk_poke1_id FOREIGN KEY (poke1_id) REFERENCES pokemon(poke_id),
+    CONSTRAINT fk_poke2_id FOREIGN KEY (poke2_id) REFERENCES pokemon(poke_id),
+    CONSTRAINT fk_poke3_id FOREIGN KEY (poke3_id) REFERENCES pokemon(poke_id),
+    CONSTRAINT fk_poke4_id FOREIGN KEY (poke4_id) REFERENCES pokemon(poke_id),
+    CONSTRAINT fk_poke5_id FOREIGN KEY (poke5_id) REFERENCES pokemon(poke_id),
+    CONSTRAINT fk_poke6_id FOREIGN KEY (poke6_id) REFERENCES pokemon(poke_id)
 );
