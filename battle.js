b89ef15c0bd2854db1d3ac8219a45a7be8af799e
Leah <143630442+LeahA12@users.noreply.github.com>
2026-06-24 00:09:00 -0400

    Drawing emptyBattle.img onto canvas

--- battle.js
+++ battle.js
@@ -1,3 +1,19 @@
 var myCanvas = document.getElementById("BattleCanvas");
 var canvasWidth = myCanvas.width;
 var canvasHeight = myCanvas.height;
+var context = myCanvas.getContext("2d");
+
+// CREATE BACKGROUND BATTLE ARENA FROM IMAGE FILE
+var battleArena = new Image();
+battleArena.src = "emptyBattle.png";
+
+context.drawImage(
+	// Source IMAGE that will be drawn into destination CANVAS
+	battleArena,
+	
+	// Source IMAGE coords and width/height
+	0, 0, battleArena.width, battleArena.height,
+	
+	// Destination CANVAS coords and width/height
+	0, 0, canvasWidth, canvasHeight
+);
