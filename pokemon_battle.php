6ffb481581934ae67ebe9d4cc847fc9d5edf6018
Leah <143630442+LeahA12@users.noreply.github.com>
2026-06-24 01:10:00 -0400

    Added a dif approach to battle ui -> overlaying text ontop of emptyBattle.jpg

--- pokemon_battle.php
+++ pokemon_battle.php
@@ -6,17 +6,38 @@
         <link rel="stylesheet" href="style.css">
 		<script async src="battle.js"></script>
     </head>
-    <body style="justify-content: center; display: flex; padding: 50px">
+    <body style="justify-content: display: flex; padding: 50px">
+		<!-- 
+			Note that emptyBattle.jpg is 1261 x 832 -> width: 1261, height: 832
+			emptyBattle.jpg seems a little big for a screen so i shrunk it by 
+			finding width/2 and height/2 which is...
+				halved width: 1261/2 = 630.5
+				halved height: 832/2 = 416
+		-->
+	
+		<!-- Leah's idea -->
+		<div class="parent">
+		  <img class="emptyBattle" src="emptyBattle.jpg" width="630" height="416"/>
+		  <div class="playerStatus">
+			<p>Player pokemon status</p> 
+		  </div>
+		  <div class="opponentStatus">
+			<p>Opponent pokemon status</p> 
+		  </div>
+		</div>
+		<br> <br> <br> <br> <br> <br>
+        <!-- Max's idea w/Leah's background canvas idea -->
 		<div class="battleBox">
-	        <div class="column playerStatus">
+	        <div class="column playerStatus_Max">
             	(Player pokemon status (look at poke showdown for what i mean))
 	        </div>
 	        <div class="column battleArena">
 				<canvas id="BattleCanvas" width="630" height="416"></canvas>
 	        </div>
-	        <div class="column opponentStatus">
+	        <div class="column opponentStatus_Max">
                 (Opponent pokemon status)
 	        </div>
         </div>
+		<br> <br> <br>
     </body>
 </html>
