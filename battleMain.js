509f11b74ec86d398768f80a93680c900bc4a4c1
Leah <143630442+LeahA12@users.noreply.github.com>
2026-06-26 12:16:50 -0400

    Splitted battle.js into battleFunctions.js & battleMain.js

---
+++ battleMain.js
@@ -0,0 +1,7 @@
+// testing drawing player & opponent sprites
+drawPlayerSprite("treecko");
+drawOppSprite("swampert");
+
+document.addEventListener('click', function () {
+	playSound('sounds/battle.mp3');
+});
