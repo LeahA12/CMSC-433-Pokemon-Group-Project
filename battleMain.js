b75e92165eae12f1921d3f137f3bdcd8964ea7b9
mjsovich <sw52457@umbc.edu>
2026-06-29 19:30:29 -0400

    test using pokemon array

--- battleMain.js
+++ battleMain.js
@@ -68,6 +68,7 @@ function battleEnd() {
     });
 }
 
+/*
 // ignore this its testing putting a variable in the html file -- will be deleted
 // var testName1 = "TOXTRICITY";
 // document.getElementById("optionText").textContent = `WHAT WILL ${testName1} DO?`;
@@ -82,3 +83,6 @@ document.getElementById("move1Button").textContent = testMoveArray[0].name;
 document.getElementById("move2Button").textContent = testMoveArray[1].name;
 document.getElementById("move3Button").textContent = testMoveArray[2].name;
 document.getElementById("move4Button").textContent = testMoveArray[3].name;
+*/
+
+document.getElementById("optionText").textContent = `WHAT WILL ${pokemon[0].name} DO?`;
